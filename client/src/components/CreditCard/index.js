373166b35ed471bc532b14d8715cdb7e7be077a8
dantalmont <dantalmont@gmail.com>
2020-10-19 14:14:33 -0400

    adjusted card spacing

--- client/src/components/CreditCard/index.js
+++ client/src/components/CreditCard/index.js
@@ -9,7 +9,7 @@ function CreditCard ({children}) {
                 <img src={creditPic} style={{height:30 , width: 50}}></img>  
                 <h4 class="fluid-text center-align" style={{ color: "black" }}>INBOX</h4>
             </div> */}
-            <div style={{ display: 'flex', alignItems: 'center' }}><img src={creditPic} style={{height:40, width:40, marginLeft:120, marginTop:65}}></img>
+            <div style={{ display: 'flex', alignItems: 'center' }}><img src={creditPic} style={{height:40, width:40, marginLeft:150, marginTop:65}}></img>
                 <h4 class="fluid-text center-align" style={{marginTop:75, color: "black"  }}>InBox</h4>
                 </div>
             <div class="card-content">
--- client/src/components/DebitCard/index.js
+++ client/src/components/DebitCard/index.js
@@ -9,7 +9,7 @@ function CreditCard ({children}) {
                     <img src={debitPic} style={{height:30, width:50}}></img>
                     <h4 class="fluid-text center-align" style={{ color: "black"  }}>INBOX</h4>
                 </div> */}
-                <div style={{ display: 'flex', alignItems: 'center' }}><img src={debitPic} style={{height:55, width:50, marginLeft:100, marginTop:45}}></img>
+                <div style={{ display: 'flex', alignItems: 'center' }}><img src={debitPic} style={{height:55, width:50, marginLeft:130, marginTop:45}}></img>
                 <h4 class="fluid-text center-align" style={{marginTop:75, color: "black"  }}>OutBox</h4>
                 </div>
                 <div class="card-content">
